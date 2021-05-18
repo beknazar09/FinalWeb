a41691425f35ef1cd3f943349f78794f22ea00ad
beknazar09 <57626729+beknazar09@users.noreply.github.com>
2021-05-18 19:10:26 +0600

    Add files via upload

---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <TaskForm/>
+    <tasks/>
+  </div>
+</template>
+
+<script>
+import tasks from './components/task.vue'
+import TaskForm from './components/TaskForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    tasks,
+    TaskForm
+  }
+}
+</script>
+
+<style>
+
+</style>
